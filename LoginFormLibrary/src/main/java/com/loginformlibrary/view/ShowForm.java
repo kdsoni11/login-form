bad0d09e0b1b1ed852e3044ada17d057e989dd22
kapil soni <kapil.soni@yusata.com>
2023-07-16 23:52:04 +0530

    add comment on code block

--- LoginFormLibrary/src/main/java/com/loginformlibrary/view/ShowForm.java
+++ LoginFormLibrary/src/main/java/com/loginformlibrary/view/ShowForm.java
@@ -10,32 +10,55 @@ import com.loginformlibrary.listener.CompleteListener;
 
 public class ShowForm {
 
-    public ShowForm() {
-    }
 
+    /**
+     * init for Load Dialog
+     * @param context  activity context
+     * @param listener call onComplete method when submit reach
+     */
     public void init(Context context, CompleteListener listener){
+
+        //Collect the instance of context else return exception
         if (context instanceof AppCompatActivity){
 
+            //Initialize ViewModel
             ViewModel viewModel = new ViewModelProvider(((AppCompatActivity) context)).get(ViewModel.class);
             viewModel.showForm(context,listener);
         }else {
             throw  new RuntimeException("Instance must be AppCompatActivity instance");
         }
     }
+
+    /**
+     * Static class of builder for binding context and complete listener
+    * */
     public static class Builder {
         private Context context;
         private CompleteListener listener;
 
+        /**
+         * binding context to parameter
+         * @param context view context
+         * @return current instance
+        * */
         public Builder with(Context context){
             this.context = context;
             return this;
         }
 
+        /**
+         * binding listener to parameter
+         * @param listener task complete listener
+         * @return current instance
+         * */
         public Builder setListener(CompleteListener listener){
             this.listener = listener;
             return this;
         }
 
+        /**
+         * init build for loading login form
+         * */
         public void build(){
             ShowForm showForm = new ShowForm();
             showForm.init(context, listener);
--- LoginFormLibrary/src/main/java/com/loginformlibrary/viewModel/ViewModel.java
+++ LoginFormLibrary/src/main/java/com/loginformlibrary/viewModel/ViewModel.java
@@ -2,6 +2,7 @@ package com.loginformlibrary.viewModel;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.view.Window;
 
@@ -13,22 +14,49 @@ import com.loginformlibrary.model.LoginFormModel;
 
 public class ViewModel extends androidx.lifecycle.ViewModel {
 
+
+    /**
+     * Load Dialog of login for
+     * @param context  activity context
+     * @param listener call onComplete method when submit reach
+     */
     public void showForm(Context context, CompleteListener listener) {
+
+        //Initialize builder instance
         AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        //Initialize layout binding
         LoginFormLayBinding binding = LoginFormLayBinding.inflate(((AppCompatActivity) context).getLayoutInflater());
+
+        //Set dialog cancelable disable
         builder.setCancelable(false);
+
+        //Set custom view on dialog box
         builder.setView(binding.getRoot());
+
+        //Create dialog box
         AlertDialog alertDialog = builder.create();
+
+        //Set dialog cancel listener
         binding.cancelBtn.setOnClickListener(v -> {
+
+            //Dismissing dialog
             alertDialog.dismiss();
+
+            //Call onComplete method of CompleteListener interface
             listener.onComplete(null);
         });
+
+        //Set dialog submit listener
         binding.submitBtn.setOnClickListener(v -> {
+
+            //Getting all text from view
             String firstName = binding.firstNameEditText.getText().toString().trim();
             String lastName = binding.lastNameEditText.getText().toString().trim();
             String mobile = binding.mobileNoEditText.getText().toString().trim();
             String emailId = binding.emailIdEditText.getText().toString().trim();
 
+            //Checking empty validation
             if (firstName.isEmpty()) {
                 binding.firstNameEditText.setError("Required");
             }
@@ -40,26 +68,41 @@ public class ViewModel extends androidx.lifecycle.ViewModel {
             }
             if (emailId.isEmpty()) {
                 binding.emailIdEditText.setError("Required");
-            } else if (!isValid(emailId)) {
+            } else if (!isValid(emailId)) {//Checking email validation
                 binding.emailIdEditText.setError("Invalid EmailId");
             }
+
+            //init when all validation comes true
             if (!firstName.isEmpty() && !lastName.isEmpty() && (!emailId.isEmpty() && isValid(emailId))) {
                 alertDialog.dismiss();
+
+                //Model initialize
                 LoginFormModel formModel = new LoginFormModel();
                 formModel.setFirstName(firstName);
                 formModel.setLastName(lastName);
                 formModel.setMobileNo(mobile);
                 formModel.setEmailId(emailId);
+
+                //passing model to interface
                 listener.onComplete(formModel);
             }
         });
+
+        //setting transparent background of Dialog box
         Window window = alertDialog.getWindow();
         if (window != null) {
-            window.setBackgroundDrawable(new ColorDrawable());
+            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         }
+
+        //Show dialog box on view
         alertDialog.show();
     }
 
+    /**
+     * isValid method is email validation
+     * @param email  email id string
+     * @return return true if email id is valid else false
+     */
     boolean isValid(String email) {
         String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
         return email.matches(regex);
